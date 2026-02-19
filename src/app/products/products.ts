afe4ff846c9e0c55a96068180bc065a360abbbb5
Aikey Shayne Abucay <abucay.aikeyshayne17@gmail.com>
2026-02-19 14:01:09 +0800

    Lab 3 - Dynamic products with Angular

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -12,11 +12,11 @@ import { FormsModule } from '@angular/forms'; // <-- add this
 export class ProductsComponent {
 
  products = [
-  { name: 'Rose Gold', price: 150, available: true, category: 'Food', image: '/rosegold.jpg' },
-  { name: 'Pearl Charm', price: 99, available: true, category: 'Food', image: '/pearlcharm.jpg' },
-  { name: 'Crystal Heart', price: 120, available: true, category: 'Drinks', image: '/crystalheart.jpeg' },
-  { name: 'Gold', price: 150, available: false, category: 'Food', image: '/gold.jpg' },
-  { name: 'Silver', price: 150, available: false, category: 'Food', image: '/silver.jpg' },
+  { name: 'Rose Gold', price: 150, available: true, category: 'Gold', image: '/rosegold.jpg' },
+  { name: 'Pearl Charm', price: 99, available: true, category: 'Necklace', image: '/pearlcharm.jpg' },
+  { name: 'Crystal Heart', price: 120, available: true, category: 'Necklace', image: '/crystalheart.jpeg' },
+  { name: 'Gold', price: 150, available: false, category: 'Gold', image: '/gold.jpg' },
+  { name: 'Silver', price: 150, available: false, category: 'Silver', image: '/silver.jpg' },
 
 ];
 
